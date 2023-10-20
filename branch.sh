cf31fd1b52b05eed581aad7b417b0265c1ec9d7d
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-20 15:49:52 +0800

    Update branch.sh

--- branch.sh
+++ branch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-html_content=$(curl -s "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git")
-branch_names=$(echo "$html_content" | grep -oP 'option value="linux-\d+\.\d+\.y"' | grep -oP 'linux-\d+\.\d+\.y')
-latest_branch=$(echo "$branch_names" | sort -Vr | head -n 1)
+
+url="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
+html=$(curl -s $url)
+
+branches=$(echo "$html" | grep -oP '/pub/scm/linux/kernel/git/stable/linux.git/log/\?h=\K[^"]*')
+
+latest_branch=$(echo "$branches" | sort -Vr | head -n 1)
+
 echo "$latest_branch"
