f77a7819a8b776acea34fdc0f888b68430638aba
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-20 14:29:49 +0800

    Update branch.sh

--- branch.sh
+++ branch.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-content=$(curl -s "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git")
-branch=$(echo "$content" | grep -oP "linux-\d+\.\d+\.y")
-latest_branch=$(echo "$branch" | sort -Vr | head -n 1)
-echo "$branch_names"
+
+# Get the HTML content of the page
+html_content=$(curl -s "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git")
+
+# Extract the branch names from the HTML content
+branch_names=$(echo "$html_content" | grep -oP "refs/heads/linux-\d+\.\d+\.y" | grep -oP "linux-\d+\.\d+\.y")
+
+# Sort the branch names and get the latest one
+latest_branch=$(echo "$branch_names" | sort -Vr | head -n 1)
+
+# Print the latest branch name
+echo "$latest_branch"
