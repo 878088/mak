b0bf6cb0bc75fbf085b09e81a2d61a12d8018fd8
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-20 15:30:47 +0800

    Update branch.sh

--- branch.sh
+++ branch.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Get the HTML content of the page
-html_content=$(curl -s "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git")
+url="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
+html=$(curl -s $url)
 
-# Extract the branch names from the HTML content
-branch_names=$(echo "$html_content" | grep -oP 'option value="\Klinux-\d+\.\d+\.y')
+branches=$(echo "$html" | grep -oP '/pub/scm/linux/kernel/git/stable/linux.git/log/\?h=\K[^"]*')
 
-# Sort the branch names and get the latest one
-latest_branch=$(echo "$branch_names" | sort -V | tail -n 1)
-
-# Print the latest branch name
-echo "$latest_branch"
+echo "$branches"
