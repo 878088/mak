eabcc38313e307ab517ef52a4bbd05c35c1d17d9
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-20 14:35:27 +0800

    Add files via upload

--- branch.sh
+++ branch.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-# Get the HTML content of the page
 html_content=$(curl -s "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git")
 
-# Extract the branch names from the HTML content
-branch_names=$(echo "$html_content" | grep -oP "refs/heads/linux-\d+\.\d+\.y" | grep -oP "linux-\d+\.\d+\.y")
+branch_names=$(echo "$html_content" | grep -oP 'option value="linux-\d+\.\d+\.y"' | grep -oP 'linux-\d+\.\d+\.y')
 
-# Sort the branch names and get the latest one
 latest_branch=$(echo "$branch_names" | sort -Vr | head -n 1)
 
-# Print the latest branch name
 echo "$latest_branch"
