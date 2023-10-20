2b0244952585c56c3a8a138420055aa230451225
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-20 16:21:35 +0800

    Add files via upload

--- branch.sh
+++ branch.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-url="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
-html=$(curl -s $url)
-
-branches=$(echo "$html" | grep -oP '/pub/scm/linux/kernel/git/stable/linux.git/log/\?h=\K[^"]*' | grep -oP 'linux-\K[^<]*' | grep -v 'linux-rolling-stable' | grep -v 'linux-rolling-lts')
-
+html=$(curl -s "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git")
+branches=$(echo "$html" | grep -oP '/pub/scm/linux/kernel/git/stable/linux.git/log/\?h=\K[^"]*' | grep -v 'linux-rolling-stable' | grep -v 'linux-rolling-lts')
 latest_branch=$(echo "$branches" | sort -Vr | head -n 1)
-
 echo "$latest_branch"
