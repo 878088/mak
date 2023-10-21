1e96e685aa1c7d3cbc5213611184331cf2e63714
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-21 09:59:15 +0800

    Update branch.sh

--- branch.sh
+++ branch.sh
@@ -1,5 +1,5 @@
 url="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
 html=$(curl -s "$url")
-branches=$(echo "$html" | grep -oP '/pub/scm/linux/kernel/git/stable/linux.git/log/\?h=\K[^<]*' | grep -v 'linux-rolling-stable' | grep -v 'linux-rolling-lts')
+branches=$(echo "$html" | grep -oP '/pub/scm/linux/kernel/git/stable/linux.git/log/\?h=\K[^<]*' | sed 's/.*>//g' | grep -v 'linux-rolling-stable' | grep -v 'linux-rolling-lts')
 latest_branch=$(echo "$branches" | sort -Vr | head -n 1)
 echo "$latest_branch"
