db8e447becd22b0a1ac271dd69fc78d028ea2815
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-21 15:33:06 +0800

    Update install.sh

--- BBRv3/install.sh
+++ BBRv3/install.sh
@@ -1,5 +1,10 @@
-API_URL="https://api.github.com/repos/878088/BBRv3/releases"
-download_urls=$(curl -s "$API_URL" | jq -r '.[].assets[].browser_download_url')
-for url in $download_urls; do
-    echo "$url"
+#!/bin/bash
+API="https://api.github.com/repos/878088/BBRv3/releases"
+urls=$(curl -s "$API" | jq -r '.[].assets[].browser_download_url')
+for url in $urls; do
+    filename=$(basename "$url")
+    architecture=$(dpkg --print-architecture "$filename" 2>/dev/null)
+    echo "File: $filename"
+    echo "Architecture: $architecture"
+    echo
 done
