f9ce330343382ae5355baaefd120a7ccfc8fc4d6
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-21 16:13:02 +0800

    Update install.sh

--- BBRv3/install.sh
+++ BBRv3/install.sh
@@ -1,14 +1,18 @@
 API="https://api.github.com/repos/878088/BBRv3/releases"
 response=$(curl -s "$API")
-download_url=$(echo "$response" | jq -r '.[].assets[] | select(.browser_download_url | contains("linux-headers") or contains("linux-image") or contains("linux-libc-dev")) | .browser_download_url')
+download_urls=$(echo "$response" | jq -r '.[].assets[] | select(.browser_download_url | contains("linux-headers") or contains("linux-image") or contains("linux-libc-dev")) | .browser_download_url')
 arch=$(dpkg --print-architecture)
 if [ "$arch" == "amd64" ]; then
-    download_url=$(echo "$download_url" | grep "amd64")
+    download_urls=$(echo "$download_urls" | grep "amd64")
 elif [ "$arch" == "arm64" ]; then
-    download_url=$(echo "$download_url" | grep "arm64")
+    download_urls=$(echo "$download_urls" | grep "arm64")
 else
     echo "Unsupported architecture: $arch"
     exit 1
 fi
-echo "Downloading from: $download_url"
-wget "$download_url"
+echo "Downloading from the following URLs:"
+echo "$download_urls"
+while read -r url; do
+    echo "Downloading: $url"
+    wget "$url"
+done <<< "$download_urls"
