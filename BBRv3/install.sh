c111bfe066a46253716248c503d4ceaea3954454
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-22 14:54:49 +0800

    Update install.sh

--- BBRv3/install.sh
+++ BBRv3/install.sh
@@ -1,3 +1,17 @@
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+PLAIN='\033[0m'
+red() {
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+green() {
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow() {
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+install_BBRv3(){
 API="https://api.github.com/repos/878088/BBRv3/releases"
 response=$(curl -s "$API")
 download_urls=$(echo "$response" | jq -r '.[].assets[] | select(.browser_download_url | contains("linux-headers") or contains("linux-image") or contains("linux-libc-dev")) | .browser_download_url')
@@ -31,3 +45,23 @@ else
     echo "没有BBRv3目录"
     exit 1
 fi
+}
+
+menu(){
+    clear
+    echo "#############################################################"
+    echo -e "#               ${RED}BBRv3 一键安装脚本${PLAIN}           #"
+    echo "#############################################################"
+    echo ""
+    echo -e " ${GREEN}1.${PLAIN} 安装 BBRv3"
+    echo -e " ${GREEN}0.${PLAIN} 退出"
+    echo ""
+    read -rp " 请输入选项 [0-6] ：" answer
+    case $answer in
+        1) install_BBRv3 ;;
+        *) red "请输入正确的选项 [0-6]！" && exit 1 ;;
+    esac
+}
+
+menu
+    
