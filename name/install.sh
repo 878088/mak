0b7f8b176fee993e8ad65f7cc545bca67df05663
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-25 21:36:01 +0800

    Update install.sh

--- name/install.sh
+++ name/install.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
-
 if [ ! -d "name" ]; then
    mkdir "name"
 fi
-
 read -p "请输入用户名: " username
 read -p "请输入令牌: " token
 read -p "请输入域名: " domain
+read -p "请输入CFDNS: " DNS
+read -p "请输入CFDNS2: " DNS2
 
-cat > name/name.sh <<EOF
+cat > name/NAMEDNS.sh <<EOF
 curl -u "$username:$token" "https://api.dev.name.com/v4/domains/$domain:setNameservers" -X POST -H 'Content-Type: application/json' --data '{"nameservers":["ns1.name.com","ns2.name.com","ns3.name.com","ns4.name.com"]}'
+EOF
+cat > name/CFDNS.sh <<EOF
+curl -u "$username:$token" "https://api.dev.name.com/v4/domains/$domain:setNameservers" -X POST -H 'Content-Type: application/json' --data '{"nameservers":["$DNS","$DNS2"]}'
 EOF
