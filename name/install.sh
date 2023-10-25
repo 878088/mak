b5d54143b5574f61924603c1b4b850177f332d9e
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-25 21:00:18 +0800

    Update install.sh

--- name/install.sh
+++ name/install.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+if [ ! -d "name" ]; then
+   mkdir "name"
+fi
+read -p "请输入用户名: " username
+read -p "请输入令牌: " token
+read -p "请输入域名: " domain
 
+cat > /name/name.sh <<EOF
+curl -u "$username:$token" "https://api.dev.name.com/v4/domains/$domain:setNameservers" -X POST -H 'Content-Type: application/json' --data '{"nameservers":["ns1.name.com","ns2.name.com","ns3.name.com","ns4.name.com"]}'
+EOF
