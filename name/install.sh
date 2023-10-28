93b8a89ac2d43d95e2520618442c17f85527edd6
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-28 20:23:54 +0800

    Update install.sh

--- name/install.sh
+++ name/install.sh
@@ -6,13 +6,13 @@ fi
 read -p "请输入用户名: " username
 read -p "请输入令牌: " token
 read -p "请输入域名: " domain
-read -p "请输入CFDNS: " DNS
-read -p "请输入CFDNS2: " DNS2
+read -p "请输入CFNS: " NS
+read -p "请输入CFNS2: " NS2
 
 cat > name/namedns.sh <<EOF
 curl -u '$username:$token' 'https://api.name.com/v4/domains/$domain:setNameservers' -X POST -H 'Content-Type: application/json' --data '{"nameservers":["ns1.name.com","ns2.name.com","ns3.name.com","ns4.name.com"]}'
 EOF
 cat > name/cfdns.sh <<EOF
-curl -u '$username:$token' 'https://api.name.com/v4/domains/$domain:setNameservers' -X POST -H 'Content-Type: application/json' --data '{"nameservers":["$DNS","$DNS2"]}'
+curl -u '$username:$token' 'https://api.name.com/v4/domains/$domain:setNameservers' -X POST -H 'Content-Type: application/json' --data '{"nameservers":["$NS","$NS2"]}'
 EOF
 chmod +x name/*.sh
