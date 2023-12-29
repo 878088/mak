b9a63658dd3ffb2275f8ef3b23a6245dcd72c22a
粑屁 <147690638+878088@users.noreply.github.com>
2023-12-30 01:05:34 +0800

    Add files via upload

--- Azure/install2.sh
+++ Azure/install2.sh
@@ -87,7 +87,8 @@ done
 read -p "请输入用户名： " USERNAME
 read -sp "请输入密码： " PASSWORD
 
-az group create --name $REGION_CODE --location $REGION_CODE && az vm create --resource-group $REGION_CODE --name $REGION_CODE --location $REGION_CODE --image $IMAGE_CODE --size $SIZE_CODE --admin-username $USERNAME --admin-password $PASSWORD --security-type Standard --public-ip-sku Basic --public-ip-address-allocation Dynamic &&az vm open-port --ids $(az vm list -g $REGION_CODE --query "[].id" -o tsv) --port '*'
+az group create --name $REGION_CODE --location $REGION_CODE && az vm create --resource-group $REGION_CODE --name $REGION_CODE --location $REGION_CODE --image $IMAGE_CODE --size $SIZE_CODE --admin-username $USERNAME --admin-password $PASSWORD --security-type Standard --public-ip-sku Basic --public-ip-address-allocation Dynami
+
 }
 
 execute
