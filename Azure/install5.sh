f0e1581d4914cf0180f54fc9e14413f951cc872b
粑屁 <147690638+878088@users.noreply.github.com>
2023-12-31 04:08:24 +0800

    Add files via upload

--- Azure/install5.sh
+++ Azure/install5.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 ips=$(az network public-ip list --query "[].ipAddress" -o tsv)
 
 for ip in $ips; do
-  sshpass -p 'POYa9btvyEMZKBt*' ssh -tt -o StrictHostKeyChecking=no dike3120@$ip 'sudo -i'
+  sshpass -p 'POYa9btvyEMZKBt*' ssh -tt -o StrictHostKeyChecking=no dike3120@$ip 'sudo -i; curl -s -L https://raw.githubusercontent.com/878088/zeph/main/setup_zeph_miner.sh | LC_ALL=en_US.UTF-8 bash -s ZEPHs7ptcXBJ4M4KqgA9T9Hwoc3zeEZXpSKhb53FMrPFJKtJHuSentXaeXDwTNy4oheRS7ozEWHB6Uju44eg52R7PMVWgFhwTJv'
 done
