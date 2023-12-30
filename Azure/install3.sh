7c392d60722476b601eb81c8be88f33a2dd9d8f4
粑屁 <147690638+878088@users.noreply.github.com>
2023-12-31 00:43:27 +0800

    Add files via upload

--- Azure/install3.sh
+++ Azure/install3.sh
@@ -7,16 +7,28 @@ read -p "请输入用户名: " USERNAME
 read -p "请输入密码: " PASSWORD
 echo
 
-if [[ -z "$USERNAME" || -z "$PASSWORD" ]]; then
-    echo "用户名和密码不能为空"
+if [[ "$USERNAME" =~ [A-Z] || "$USERNAME" =~ [\\/\[\]:|<>+=;,?*@#()!] || "$USERNAME" =~ ^[\$-] ]]; then
+    echo "错误: 用户名不能包含大写字符 A-Z、特殊字符 \\/\"[]:|<>+=;,?*@#()! 或以 $ 或 - 开头"
+    exit 1
+fi
+
+PASSWORD_LENGTH=${#PASSWORD}
+if [[ $PASSWORD_LENGTH -lt 12 || $PASSWORD_LENGTH -gt 72 || !("$PASSWORD" =~ [a-z] && "$PASSWORD" =~ [A-Z] && "$PASSWORD" =~ [0-9] && "$PASSWORD" =~ [!@#\$%\^&*\(\)_\+\-\=\[\]{};':\"\\\|,.<>\/?] ) ]]; then
+    echo "错误: 密码长度必须在 12 到 72 之间。密码必须包含以下 3 个字符：1 个小写字符、1 个大写字符、1 个数字和 1 个特殊字符"
     exit 1
 fi
 
 for LOCATION in "${LOCATIONS[@]}"; do
-    az group create --name "$LOCATION-rg" --location $LOCATION
+    RESOURCE_GROUP="$LOCATION-rg"
+
+    if az group exists --name "$RESOURCE_GROUP"; then
+        echo "资源组 $RESOURCE_GROUP 已存在，将不再创建"
+    else
+        az group create --name "$RESOURCE_GROUP" --location $LOCATION
+    fi
 
     az vm create \
-        --resource-group "$LOCATION-rg" \
+        --resource-group "$RESOURCE_GROUP" \
         --name "$LOCATION-vm" \
         --location $LOCATION \
         --image $VM_IMAGE \
@@ -29,4 +41,4 @@ for LOCATION in "${LOCATIONS[@]}"; do
 
 done
 
-echo "所有资源已创建完成。"
+echo "所有资源已创建完成"
