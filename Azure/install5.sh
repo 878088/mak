11be68afedf87aee5d3b749e2734f3e914e3b1fe
粑屁 <147690638+878088@users.noreply.github.com>
2023-12-31 03:59:20 +0800

    Update install5.sh

--- Azure/install5.sh
+++ Azure/install5.sh
@@ -2,5 +2,5 @@
 ips=$(az network public-ip list --query "[].ipAddress" -o tsv)
 
 for ip in $ips; do
-  sshpass -p 'POYa9btvyEMZKBt*' ssh -o StrictHostKeyChecking=no dike3120@$ip 'echo "POYa9btvyEMZKBt*" | sudo -S bash'
+  sshpass -p 'POYa9btvyEMZKBt*' ssh -o StrictHostKeyChecking=no dike3120@$ip 'sudo -S bash'
 done
