6ff690ae3ffc7fa3020f7fa9e1b341d033d850b3
粑屁 <147690638+878088@users.noreply.github.com>
2023-12-31 10:49:43 +0800

    Update install.sh

--- Azure/install.sh
+++ Azure/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 
 install_azure() {
     os=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
@@ -7,30 +10,30 @@ install_azure() {
         sudo apt-get update -y
         sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg jq sshpass screen -y
 
-        echo "检查目录是否存在"
+        echo -e "${GREEN}检查目录是否存在${NC}"
         if [ ! -d "/etc/apt/keyrings" ]; then
-            echo "目录不存在，现在创建"
+            echo -e "${RED}目录不存在，现在创建${NC}"
             sudo mkdir -p /etc/apt/keyrings
         else
-            echo "目录已存在"
+            echo -e "${GREEN}目录已存在${NC}"
         fi
         
-        echo "下载并安装 Microsoft 签名密钥"
+        echo -e "${GREEN}下载并安装 Microsoft 签名密钥${NC}"
         curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
         gpg --dearmor |
         sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
         sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
 
-        echo "添加 Azure CLI 软件存储库"
+        echo -e "${GREEN}添加 Azure CLI 软件存储库${NC}"
         AZ_DIST=$(lsb_release -cs)
         echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_DIST main" |
         sudo tee /etc/apt/sources.list.d/azure-cli.list
 
-        echo "更新存储库信息并安装 Azure CLI 包"
+        echo -e "${GREEN}更新存储库信息并安装 Azure CLI 包${NC}"
         sudo apt-get update -y
         sudo apt-get install azure-cli -y
     else
-        echo "没有适配系统"
+        echo -e "${RED}没有适配系统${NC}"
     fi
 }
 
@@ -38,20 +41,20 @@ login() {
     if command -v az > /dev/null 2>&1; then
         output=$(az login --use-device-code)
         if echo "$output" | jq -e . > /dev/null 2>&1; then
-            echo "登录成功"
+            echo -e "${GREEN}登录成功${NC}"
         else
-            echo "登录失败，请重试"
+            echo -e "${RED}登录失败，请重试${NC}"
         fi
     else
-        echo "未安装 Azure CLI 请先安装"
+        echo -e "${RED}未安装 Azure CLI 请先安装${NC}"
     fi
 }
 
 menu() {
-    echo "1. Install Azure CLI"
-    echo "2. Login to Azure"
-    echo "3. Exit"
-    read -p "Enter your choice: " choice
+    echo -e "${GREEN}1. 安装 Azure CLI${NC}"
+    echo -e "${GREEN}2. 登录 Azure CLI${NC}"
+    echo -e "${GREEN}0. 退出${NC}"
+    read -p "输入您的选择: " choice
 
     case $choice in
         1)
@@ -60,12 +63,12 @@ menu() {
         2)
             login
             ;;
-        3)
-            echo "Exiting..."
+        0)
+            echo -e "${RED}退出...${NC}"
             exit 1
             ;;
         *)
-            echo "Invalid choice"
+            echo -e "${RED}选择无效${NC}"
             menu
             ;;
     esac
