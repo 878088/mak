13f86b6f31745335950b8d63d6f4637a535707cf
粑屁 <147690638+878088@users.noreply.github.com>
2024-01-01 17:44:58 +0800

    Update install.sh

--- Azure/install.sh
+++ Azure/install.sh
@@ -108,7 +108,7 @@ if ! echo "$PASSWORD" | grep -q '[0-9]'; then
 fi
 
 if ! echo "$PASSWORD" | grep -q '[~`!@#$%^&*()-=_+[\]{}|;:",.<>/?]'; then
-    echo -e "\e[32m错误: 密码必须包含至少一个特殊字符。支持[~`!@#$%^&*()-=_+[\]{}|;:",.<>/?]\e[0m"
+    echo -e "\e[32m错误: 密码必须包含至少一个特殊字符。\e[0m"
     continue
 fi
     echo -e
