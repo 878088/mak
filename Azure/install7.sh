e035300401c0600e3e9bb77da07bbe9476c8d455
粑屁 <147690638+878088@users.noreply.github.com>
2024-01-08 15:40:42 +0800

    Update install7.sh

--- Azure/install7.sh
+++ Azure/install7.sh
@@ -2,27 +2,20 @@
 
 LOCATIONS=("westus3" "australiaeast" "uksouth" "southeastasia" "swedencentral" "centralus" "centralindia" "eastasia" "japaneast" "koreacentral" "canadacentral" "francecentral" "germanywestcentral" "italynorth" "norwayeast" "polandcentral" "switzerlandnorth" "brazilsouth" "northcentralus" "westus" "japanwest" "australiacentral" "canadaeast" "ukwest" "southcentralus" "northeurope" "southafricanorth" "australiasoutheast" "southindia" "uaenorth")
 
-while true; do
-    allGroupsCreated=true
-    
-    for location in "${LOCATIONS[@]}"; do
-        groupInfo=$(az group show --name "$location" 2>&1)
+for location in "${LOCATIONS[@]}"; do
+    groupInfo=$(az group show --name "$location" 2>&1)
+    if [ $? -eq 0 ]; then
+        echo -e "\e[33m资源组已存在 $location\e[0m"
+    else
+        errorMessage=$(echo "$groupInfo" | grep -oP "(?<=Message:\s).+")
+        az group create --name "$location" --location "$location"
         if [ $? -eq 0 ]; then
-            echo -e "\e[33m资源组已存在 $location\e[0m"
+            echo -e "\e[32m资源组创建成功 $location\e[0m"
+            nohup az vm create --resource-group "$location" --name southindia --location "$location" --image Debian11 --size Standard_DS12_v2 --admin-username ooo --admin-password KKKKjjjj520. --security-type Standard --public-ip-sku Basic --public-ip-address-allocation Dynamic > /dev/null 2>&1 &
+            echo -e "\e[36m已在后台执行 az vm create 命令\e[0m"
         else
-            errorMessage=$(echo "$groupInfo" | grep -oP "(?<=Message:\s).+")
-            az group create --name "$location" --location "$location"
-            if [ $? -eq 0 ]; then
-                echo -e "\e[32m资源组创建成功 $location\e[0m"
-            else
-                echo -e "\e[31m资源组创建失败 $location\e[0m"
-                echo -e "\e[31m$errorMessage\e[0m"
-                allGroupsCreated=false
-            fi
+            echo -e "\e[31m资源组创建失败 $location\e[0m"
+            echo -e "\e[31m$errorMessage\e[0m"
         fi
-    done
-    
-    if $allGroupsCreated; then
-        break
     fi
 done
