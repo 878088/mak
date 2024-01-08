f8fb1cd53a1d5741adf72e22bae015d0f92694d7
粑屁 <147690638+878088@users.noreply.github.com>
2024-01-08 14:15:55 +0800

    Update install7.sh

--- Azure/install7.sh
+++ Azure/install7.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 LOCATIONS=("westus3" "australiaeast" "uksouth" "southeastasia" "swedencentral" "centralus" "centralindia" "eastasia" "japaneast" "koreacentral" "canadacentral" "francecentral" "germanywestcentral" "italynorth" "norwayeast" "polandcentral" "switzerlandnorth" "brazilsouth" "northcentralus" "westus" "japanwest" "australiacentral" "canadaeast" "ukwest" "southcentralus" "northeurope" "southafricanorth" "australiasoutheast" "southindia" "uaenorth")
-
 while true; do
     for location in "${LOCATIONS[@]}"; do
         az group create --name "$location" --location "$location"
@@ -9,6 +7,10 @@ while true; do
             echo "资源组创建成功 $location"
         else
             echo "资源组创建失败 $location"
+            allGroupsCreated=false
         fi
-    done
+    done    
+    if $allGroupsCreated; then
+        break
+    fi
 done
