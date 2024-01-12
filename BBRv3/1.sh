7b9ee08a82ee301bd82ef4b4a604b6ee4a14d13f
粑屁 <147690638+878088@users.noreply.github.com>
2024-01-12 13:54:37 +0800

    Update 1.sh

--- BBRv3/1.sh
+++ BBRv3/1.sh
@@ -4,5 +4,5 @@ URL="https://mirrors.edge.kernel.org/debian/pool/main/l/linux/"
 
 wget -qO- $URL | 
 grep -Po '(?<=href=")[^"]*(?=")' | 
-grep "linux-image-6.7-cloud｜unsigned" |
+grep "linux-image-6.7-cloud\|unsigned" |
 sed "s|^|$URL|"
